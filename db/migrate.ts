9ae84d9c6063e8dcd16bad18697ed2da0b132b9c
OKTAY YILMAZ <oktayyilmaz090.work@gmail.com>
2025-09-24 07:41:56 +0300

    Implement code changes to enhance functionality and improve performance

--- db/migrate.ts
+++ db/migrate.ts
@@ -3,9 +3,9 @@ import db, { connection } from ".";
 import config from "../drizzle.config"
 
 if (!process.env.DB_MIGRATING) {
-    throw new Error("DB_MIGRATING must be set");
+	throw new Error("DB_MIGRATING must be set");
 }
 
 migrate(db, { migrationsFolder: config.out! })
-    .then(() => connection.end())
-    .then(() => console.log("Migrated successfully"))
+	.then(() => connection.end())
+	.then(() => console.log("Migrated successfully"))
